ce65addf42b5fde0cab2e57df6d9b5aee4d0f31a
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-23 22:04:05 +0900

    wip(head-only-ssr): L@E logic

--- head-only-ssr/lambda/origin-response/index.ts
+++ head-only-ssr/lambda/origin-response/index.ts
@@ -1,14 +1,37 @@
-import { Handler, CloudFrontResponseEvent } from "aws-lambda"
+import { CloudFrontResponse, CloudFrontResponseHandler } from "aws-lambda"
 
 declare global {
   const __SITE_ROOT_INDEX_HTML_CONTENT: string
 }
 
-export const handler: Handler<CloudFrontResponseEvent> = async (event) => {
-  const response = event.Records[0].cf.response
-  console.log(JSON.stringify({ response }))
-  console.log(JSON.stringify({ request: event.Records[0].cf.request }))
-  console.log(__SITE_ROOT_INDEX_HTML_CONTENT)
+export const handler: CloudFrontResponseHandler = async (event) => {
+  const response = event.Records[0].cf.response as CloudFrontResponse & { body: string }
+  const request = event.Records[0].cf.request
+
+  const maybeHTML = request.uri.endsWith(".html") || !request.uri.includes(".")
+  if (maybeHTML) {
+    const jstNow = new Date(Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000)
+
+    const description: string = (() => {
+      if (request.uri === "/") return "root page"
+      const id = request.uri.match(/\/([\d]+)/)?.[1]
+      if (id) return `page ${id}`
+      return "unknown page"
+    })()
+
+    response.body = __SITE_ROOT_INDEX_HTML_CONTENT.replace(
+      "<!-- PLACEHOLDER -->",
+      `
+      <meta name="description" content="${description + " " + jstNow.toLocaleString()}">
+      `
+    )
+    response.headers["content-type"] = [{ key: "Content-Type", value: "text/html" }]
+  }
+
+  // JS, CSS, images, etc. have a content hash attached by the bundler, so the cache age can be extended
+  response.headers["cache-control"] = [
+    { key: "Cache-Control", value: `max-age=${maybeHTML ? 600 : 60 * 60 * 24 * 365}` },
+  ]
 
   return response
 }
