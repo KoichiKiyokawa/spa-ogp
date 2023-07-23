190d55217666c5cef2449be4e7c50850f0927356
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-23 17:28:20 +0900

    feat(head-only-ssr): use cloudflare domain

--- dynamic-rendering/infra/lib/dynamic-rendering-stack.ts
+++ dynamic-rendering/infra/lib/dynamic-rendering-stack.ts
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-import { CfnOutput, Duration, RemovalPolicy, Stack } from "aws-cdk-lib";
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
-import * as iam from "aws-cdk-lib/aws-iam";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import * as logs from "aws-cdk-lib/aws-logs";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as targets from "aws-cdk-lib/aws-route53-targets";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-import { Construct } from "constructs";
+import { CfnOutput, Duration, RemovalPolicy, Stack } from "aws-cdk-lib"
+import * as acm from "aws-cdk-lib/aws-certificatemanager"
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront"
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins"
+import * as iam from "aws-cdk-lib/aws-iam"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as route53 from "aws-cdk-lib/aws-route53"
+import * as targets from "aws-cdk-lib/aws-route53-targets"
+import * as s3 from "aws-cdk-lib/aws-s3"
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment"
+import { Construct } from "constructs"
 
 export interface StaticSiteProps {
-  domainName: string;
-  siteSubDomain: string;
+  domainName: string
+  siteSubDomain: string
 }
 
 /**
@@ -26,21 +26,21 @@ export interface StaticSiteProps {
  */
 export class StaticSite extends Construct {
   constructor(parent: Stack, name: string, props: StaticSiteProps) {
-    super(parent, name);
+    super(parent, name)
 
     const zone = route53.HostedZone.fromLookup(this, "Zone", {
       domainName: props.domainName,
-    });
-    const siteDomain = props.siteSubDomain + "." + props.domainName;
+    })
+    const siteDomain = props.siteSubDomain + "." + props.domainName
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
       this,
       "cloudfront-OAI",
       {
         comment: `OAI for ${name}`,
       }
-    );
+    )
 
-    new CfnOutput(this, "Site", { value: "https://" + siteDomain });
+    new CfnOutput(this, "Site", { value: "https://" + siteDomain })
 
     // Content bucket
     const siteBucket = new s3.Bucket(this, "SiteBucket", {
@@ -60,7 +60,7 @@ export class StaticSite extends Construct {
        * setting will enable full cleanup of the demo.
        */
       autoDeleteObjects: true, // NOT recommended for production code
-    });
+    })
 
     // Grant access to cloudfront
     siteBucket.addToResourcePolicy(
@@ -73,16 +73,21 @@ export class StaticSite extends Construct {
           ),
         ],
       })
-    );
-    new CfnOutput(this, "Bucket", { value: siteBucket.bucketName });
+    )
+    new CfnOutput(this, "Bucket", { value: siteBucket.bucketName })
 
     // TLS certificate
-    const certificate = new acm.Certificate(this, "SiteCertificate", {
-      domainName: siteDomain,
-      validation: acm.CertificateValidation.fromDns(zone),
-    });
+    // const certificate = new acm.Certificate(this, "SiteCertificate", {
+    //   domainName: siteDomain,
+    //   validation: acm.CertificateValidation.fromDns(zone),
+    // });
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      "SiteCertificate",
+      "arn:aws:acm:us-east-1:877131159332:certificate/ba35c31d-d07d-411f-b297-2ba3901208c8"
+    )
 
-    new CfnOutput(this, "Certificate", { value: certificate.certificateArn });
+    // new CfnOutput(this, "Certificate", { value: certificate.certificateArn })
 
     // DynamicRender Lambda
     const viewerRequestLambda = new NodejsFunction(this, "ViewerRequest", {
@@ -90,7 +95,7 @@ export class StaticSite extends Construct {
       handler: "handler",
       runtime: lambda.Runtime.NODEJS_18_X,
       logRetention: logs.RetentionDays.ONE_WEEK,
-    });
+    })
     const originRequestLambda = new NodejsFunction(this, "OriginRequest", {
       entry: "../lambda/origin-request/index.ts",
       handler: "handler",
@@ -98,7 +103,7 @@ export class StaticSite extends Construct {
       memorySize: 4096,
       timeout: Duration.seconds(30),
       logRetention: logs.RetentionDays.ONE_WEEK,
-    });
+    })
 
     // CloudFront distribution
     const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
@@ -134,20 +139,25 @@ export class StaticSite extends Construct {
           },
         ],
       },
-    });
+    })
 
     new CfnOutput(this, "DistributionId", {
       value: distribution.distributionId,
-    });
+    })
+    new CfnOutput(this, "DistributionDomainName", {
+      value: distribution.distributionDomainName,
+    })
 
+    // Comment out because we use CloudFlare
+    //
     // Route53 alias record for the CloudFront distribution
-    new route53.ARecord(this, "SiteAliasRecord", {
-      recordName: siteDomain,
-      target: route53.RecordTarget.fromAlias(
-        new targets.CloudFrontTarget(distribution)
-      ),
-      zone,
-    });
+    // new route53.ARecord(this, "SiteAliasRecord", {
+    //   recordName: siteDomain,
+    //   target: route53.RecordTarget.fromAlias(
+    //     new targets.CloudFrontTarget(distribution)
+    //   ),
+    //   zone,
+    // })
 
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
@@ -155,6 +165,6 @@ export class StaticSite extends Construct {
       destinationBucket: siteBucket,
       distribution,
       distributionPaths: ["/*"],
-    });
+    })
   }
 }
